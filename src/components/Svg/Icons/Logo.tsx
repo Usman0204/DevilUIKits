f93d6fe298dfd8387c4e6a872bee0543dd93518e
Waleed Shafiq <waleed.shafiq96@gmail.com>
2021-06-15 14:34:23 +0500

    test

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -5,8 +5,8 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 32 32" {...props}>
-    <image width="32" height="32" href="'images/devil/logo.png'"/>
-  </Svg>
+      <image width="32" height="32" href="'images/devil/logo.png'" />
+    </Svg>
   );
 };
 
